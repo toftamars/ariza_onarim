531f52b4c502b55f87bac8ba8b98c46754d3ebc8
toftamars <alpertofta@hotmail.com>
2025-05-26 18:52:29 +0300

    Arıza Onarım modülünde son değişiklikler: yazdırma butonu sadeleştirildi, SMS ve transfer entegrasyonları güncellendi, rapor ve görünüm iyileştirildi, erişim ve ikon sorunları giderildi.

--- ariza_onarim/models/stock.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
-
-    ariza_kayit_id = fields.Many2one('ariza.kayit', string='Arıza Kaydı', tracking=True) 
