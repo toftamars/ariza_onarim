f3bb6c5662efbe33f9a53805618023d1195c2a47
toftamars <GitHub E-posta Adresiniz>
2025-07-16 22:52:04 +0300

    Fatura kalemi alanında sadece ürün adı gösterilecek şekilde düzenlendi

--- ariza_onarim/models/__init__.py
+++ ariza_onarim/models/__init__.py
@@ -1,3 +1,4 @@
 from . import ariza
 from . import stock_picking
-from . import hr_employee 
+from . import hr_employee
+from . import account_move_line 
---
+++ ariza_onarim/models/account_move_line.py
@@ -0,0 +1,17 @@
+from odoo import models, api
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    def name_get(self):
+        """Fatura kalemi alanında sadece ürün adını göster"""
+        result = []
+        for record in self:
+            if record.product_id:
+                # Sadece ürün adını göster
+                name = record.product_id.name
+            else:
+                # Ürün yoksa varsayılan name_get davranışını kullan
+                name = super(AccountMoveLine, record).name_get()[0][1] if record.id else ''
+            result.append((record.id, name))
+        return result 
