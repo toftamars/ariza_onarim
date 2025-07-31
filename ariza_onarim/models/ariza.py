8d5f07af35a2ecfada6e1e9b3a89c60598ca8827
toftamars <GitHub E-posta Adresiniz>
2025-08-01 01:45:12 +0300

    Teslim Al butonu ile oluşturulan transferlerde edespatch_delivery_type alanı printed (Matbu) olarak ayarlandı

--- ariza_onarim/models/ariza.py
+++ ariza_onarim/models/ariza.py
@@ -1512,7 +1512,7 @@ Arıza Kaydı Tamamlandı.<br/>
             'analytic_account_id': self.analitik_hesap_id.id if self.analitik_hesap_id else False,
             'scheduled_date': fields.Datetime.now(),
             'date': fields.Datetime.now(),
-            'delivery_type': 'fixed',
+            'edespatch_delivery_type': 'printed',
         }
         
         # Teknik servise göre partner_id ayarla
@@ -1559,8 +1559,8 @@ Arıza Kaydı Tamamlandı.<br/>
         # Tamir Alımlar transferini oluştur
         tamir_alim_transfer = self.env['stock.picking'].create(picking_vals)
         
-        # Teslimat türünü fixed olarak ayarla
-        tamir_alim_transfer.write({'delivery_type': 'fixed'})
+        # Teslimat türünü Matbu olarak ayarla
+        tamir_alim_transfer.write({'edespatch_delivery_type': 'printed'})
         
         # Transfer satırını oluştur
         move_vals = {
