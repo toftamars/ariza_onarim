0420bd26400d8e9efc5c1f446a10060f632b51ec
toftamars <toftamars@users.noreply.github.com>
2026-08-20 00:10:27 +0300

    feat: Seri No alanı zorunlu yapıldı

--- ariza_onarim/__manifest__.py
+++ ariza_onarim/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Arıza Onarım',
-    'version': '1.0.13',
+    'version': '1.0.14',
     'category': 'Services',
     'summary': 'Arıza kayıt ve onarım takibi',
     'description': """
